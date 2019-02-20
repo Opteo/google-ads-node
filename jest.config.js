f7b38fda33c4ea7ac74a6cc484fa5c6bc4e6af83
Louis Bluhm <suspiciouspotato@gmail.com>
2019-02-20 11:45:20 +0000

    added: ExceptionInterceptor test

--- jest.config.js
+++ jest.config.js
@@ -27,9 +27,7 @@ module.exports = {
   // coverageDirectory: null,
 
   // An array of regexp pattern strings used to skip coverage collection
-  // coveragePathIgnorePatterns: [
-  //   "/node_modules/"
-  // ],
+  coveragePathIgnorePatterns: ["/node_modules/", "/protos/", "jest.config.js"],
 
   // A list of reporter names that Jest uses when writing coverage reports
   // coverageReporters: [
--- src/lib/client.spec.ts
+++ src/lib/client.spec.ts
@@ -1,10 +1,13 @@
 import grpc from "grpc";
+
 import GoogleAdsClient from "./client";
+import { SearchGoogleAdsRequest } from "./types";
 
 test("GoogleAdsClient", () => {
   const client = new GoogleAdsClient({
     access_token: "123",
     developer_token: "123",
+    login_customer_id: "abc123",
   });
   expect(client).toBeInstanceOf(GoogleAdsClient);
 });
@@ -16,7 +19,23 @@ test("getService", () => {
   });
 
   expect(() => client.getService("NonExistentService")).toThrowError();
+  expect(() => client.getService("GoogleAdssService")).toThrowError();
 
   const service = client.getService("GoogleAdsService");
   expect(service).toBeInstanceOf(grpc.Client);
+});
+
+test("ExceptionInterceptor", done => {
+  const client = new GoogleAdsClient({
+    access_token: "123",
+    developer_token: "123",
+  });
+
+  const service = client.getService("GoogleAdsService");
+  const request = new SearchGoogleAdsRequest();
+
+  service.search(request, (err: Error) => {
+    expect(err.message).toContain("16 UNAUTHENTICATED");
+    done();
+  });
 });
