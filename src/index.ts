6edb4a4853b37e4c3f3a9f4069048f4dcf53a033
Louis Bluhm <suspiciouspotato@gmail.com>
2020-03-06 15:57:46 +0000

    chore(exports): exposed ClientReadableStream from grpc

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
+// Core
 export * from "./lib/client";
 export * from "./lib/types";
+
+// Utils
 export { getFieldMask } from "./lib/utils";
 export { LogOptions, RequestLog } from "./lib/logger";
+export { ClientReadableStream } from "grpc";
