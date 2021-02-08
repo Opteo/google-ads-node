ef6ccf7eac99227050bda5b8049cc445189edd24
Louis Bluhm <suspiciouspotato@gmail.com>
2021-02-08 12:21:17 +0100

    removed long hack

--- init.sh
+++ init.sh
@@ -9,10 +9,3 @@ rm -rf build/test build/system-test
 
 # copy the build up to the main package directory
 cp -r build ../../
-
-# horrible hack to prevent long.js from being removed
-# please suggest a better solution if you know how
-cd ../../
-sed -i '.bak' '/var $Reader = $protobuf.Reader, $Writer = $protobuf.Writer, $util = $protobuf.util;/a \
-$util.Long = null; \
-' build/protos/protos.js
